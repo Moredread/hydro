4ec59814ee790518d31cb0c3ffef9b9a54db27dc
Eduard Bopp <eduard.bopp@aepsil0n.de>
2013-11-14 21:25:34 +0100

    Make test suite accept command line parameters

--- src/test_suite.cpp
+++ src/test_suite.cpp
@@ -1,15 +1,21 @@
 #include <cppunit/extensions/TestFactoryRegistry.h>
 #include <cppunit/ui/text/TestRunner.h>
 
+#include <string>
 
-int main( int argc, char **argv)
+
+int main(int argc, char **argv)
 {
     // Set up runner and factory registry
     CppUnit::TextUi::TestRunner runner;
     CppUnit::TestFactoryRegistry &registry = CppUnit::TestFactoryRegistry::getRegistry();
     runner.addTest(registry.makeTest());
     // Run the test suite
-    bool success = runner.run("", false);
+    std::string test_name = "";
+    if (argc == 2) {
+        test_name = argv[1];
+    }
+    bool success = runner.run(test_name, false);
     // Return suitable exit code
     return success ? 0 : 1;
 }
