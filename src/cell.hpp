fa933362603798c20587399f510aef2fbf3a5d8f
Eduard Bopp <eduard.bopp@aepsil0n.de>
2013-11-14 13:23:05 +0100

    Make a trivial cell class
    This really does not do a lot, it's just a dummy for now.

---
+++ src/cell.hpp
@@ -0,0 +1,17 @@
+#ifndef __CELL_HPP
+#define __CELL_HPP
+
+struct Cell {
+
+    double foo;
+    double bar;
+
+    Cell(double foo = 0.0, double bar = 0.0) {
+        this->foo = foo;
+        this->bar = bar;
+    }
+
+};
+
+#endif
+
