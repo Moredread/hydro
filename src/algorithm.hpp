f22c71ff7850173bc4942965db1815dab7e6adf2
Eduard Bopp <eduard.bopp@aepsil0n.de>
2013-11-14 14:16:14 +0100

    Modify pseudo-algorithms expected data structure
    The new scheme fits better with the data structure in mind.

--- src/algorithm.hpp
+++ src/algorithm.hpp
@@ -10,17 +10,24 @@ const int repeat = 1;
  * This template function implements a pseudo algorithm for testing and
  * benchmarking purposes. It takes two cells as arguments and returns a
  * transition between these types.
+ *
+ * NOTE: the algorithm itself is utmost non-sense. It only serves the purpose
+ * of performing some floating point operations.
  */
 template <class CellType, class TransitionType>
-TransitionType pseudo_advect(CellType cell1, CellType cell2)
+void pseudo_advect(const CellType &cell1, const CellType &cell2,
+    TransitionType *transition1, TransitionType *transition2)
 {
     double dfoo, dbar;
     for (int i = 0; i < repeat; i++) {
         dfoo = (cell2.foo - cell1.foo) /
                       (1.0 + pow(cell2.bar, 2) + pow(cell1.bar, 2));
-        dbar = cell1.bar * 0.01;
+        dbar = pow(cell1.bar / cell2.bar, 0.1);
     }
-    return TransitionType(dfoo, dbar);
+    transition1->foo += dfoo;
+    transition2->foo -= dfoo;
+    transition1->bar /= dbar;
+    transition2->bar *= dbar;
 }
 
 #endif
--- src/test_algorithm.cpp
+++ src/test_algorithm.cpp
@@ -1,5 +1,6 @@
 #include <cppunit/TestFixture.h>
 #include <cppunit/extensions/HelperMacros.h>
+#include <cmath>
 
 #include "cell.hpp"
 #include "algorithm.hpp"
@@ -24,9 +25,12 @@ public:
 
     void test_algorithm()
     {
-        Cell result = pseudo_advect<Cell, Cell>(cell1, cell2);
-        CPPUNIT_ASSERT_DOUBLES_EQUAL(1./6., result.foo, 1e-8);
-        CPPUNIT_ASSERT_DOUBLES_EQUAL(0.01, result.bar, 1e-8);
+        Cell trans1(0.0, 1.0), trans2(0.0, 1.0);
+        pseudo_advect<Cell, Cell>(cell1, cell2, &trans1, &trans2);
+        CPPUNIT_ASSERT_DOUBLES_EQUAL(1./6., trans1.foo, 1e-8);
+        CPPUNIT_ASSERT_DOUBLES_EQUAL(-1./6., trans2.foo, 1e-8);
+        CPPUNIT_ASSERT_DOUBLES_EQUAL(pow(2.0, 0.1), trans1.bar, 1e-8);
+        CPPUNIT_ASSERT_DOUBLES_EQUAL(pow(2.0, -0.1), trans2.bar, 1e-8);
     }
 };
 
