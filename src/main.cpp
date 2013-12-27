1abdf7237a2c18c9a1ad1d095cd256ea7c78e483
Andre-Patrick Bubel <code@andre-bubel.de>
2013-12-27 16:15:46 +0100

    Measure setup and run time of main

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
 #include <random>
+#include <iostream>
 
 #include "cell.hpp"
 #include "algorithm.hpp"
 #include "scheme.hpp"
 #include "domain.hpp"
-
+#include "timer.hpp"
 
 using namespace hydro;
 
@@ -18,13 +19,25 @@ typedef CartesianDomain1D<Cell> DomainType;
  * Main routine.
  */
 int main() {
+    CTimer setup_timer;
+    CTimer run_timer;
+
     // setup
+    setup_timer.start();
     std::default_random_engine gen;
     std::uniform_real_distribution<double> dist(0.0, 1.0);
     DomainType domain;
     SchemeType scheme(AlgorithmType(1));
     for (int i = 0; i < 10; i++)
         domain.add_cell(Cell(dist(gen), pow(2.0, dist(gen))));
+    setup_timer.stop();
+
+    run_timer.start();
     scheme.advance(domain, 1.0);
+    run_timer.stop();
+
+    std::cout << "Setup time: " << setup_timer.get_seconds() << std::endl;
+    std::cout << "Run time: " << run_timer.get_seconds() << std::endl;
+
     return 0;
 }
